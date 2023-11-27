21bd05a396b109fe0c45a6d3b35dc0e671302b99
33-66 <simon.nganga@student.moringaschool.com>
2023-11-27 13:40:42 +0300

    work completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,32 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-function Question({ question, onAnswered }) {
+const Question = ({ question, onAnswered }) => {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
 
-  // add useEffect code
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(prevTime => prevTime - 1);
+      } else {
+        handleAnswer(false); // Timer ran out, answer is false
+      }
+    }, 1000);
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeRemaining]);
 
-  const { id, prompt, answers, correctIndex } = question;
+  const handleAnswer = (answer) => {
+    if (selectedAnswer === null) {
+      setSelectedAnswer(answer);
+      setTimeRemaining(10); // Reset the timer
+      onAnswered(answer);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
-    <>
-      <h1>Question {id}</h1>
-      <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
-        return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-            {answer}
-          </button>
-        );
-      })}
-      <h5>{timeRemaining} seconds remaining</h5>
-    </>
+    <div>
+      <h2>{question.prompt}</h2>
+      <p>{timeRemaining} seconds remaining</p>
+      <ul>
+        {question.answers.map((answer, index) => (
+          <li key={index}>
+            <button onClick={() => handleAnswer(answer)} disabled={selectedAnswer !== null}>
+              {answer}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default Question;
